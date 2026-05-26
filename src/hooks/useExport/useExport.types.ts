46a868d3ea394a0c8c2b6a8a1d6b3b0b7c51d1c1
Olayiwola Akinnagbe <40915435+Olayiwola72@users.noreply.github.com>
2026-05-26 20:37:53 +0100

    Feature/prerender (#27)
    * prerender
    
    * prerender
    
    * prerender

--- src/hooks/useExport/useExport.types.ts
+++ src/hooks/useExport/useExport.types.ts
@@ -2,5 +2,5 @@ import type { ExportFormat } from '../../services/exportService/export.types'
 
 export interface ExportApi {
   exportingFormat: ExportFormat | null
-  exportAs: (format: ExportFormat, element: HTMLElement | null, customFilename?: string, captionTextColor?: string, captionBgColor?: string, captionAlignment?: 'left' | 'center' | 'right') => Promise<void>
+  exportAs: (format: ExportFormat, element: HTMLElement | null, customFilename?: string, captionTextColor?: string, captionBgColor?: string, captionAlignment?: 'left' | 'center' | 'right', captionItalic?: boolean) => Promise<void>
 }
